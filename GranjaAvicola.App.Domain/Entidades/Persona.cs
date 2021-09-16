a0ab0a37e7946f208fc414cbe67fe069cc8470ce
Miguel Ángel Reyes Malvehy <reyesmalvehy@gmail.com>
2021-09-16 12:58:55 -0500

    Creacion de la carpeta Entidades

---
+++ GranjaAvicola.App.Domain/Entidades/Persona.cs
@@ -0,0 +1,12 @@
+using System;
+namespace GranjaAvicola.App.Domain
+{
+    public class Persona
+    {
+        public int id  {get;set;}
+        public string Nombre {get;set;}
+        public string Apellido {get;set;}
+        public string Telefono {get;set;}
+    }
+    
+}
