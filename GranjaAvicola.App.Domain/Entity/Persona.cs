a122617943525e35f6307bbc2600cc946ea7ab2d
Angel Gonzalez <angonzalezpa@unal.edu.co>
2021-09-16 23:07:30 -0500

    Implementar la entidad Galpon y correccion de entidades

--- GranjaAvicola.App.Domain/Entity/Persona.cs
+++ GranjaAvicola.App.Domain/Entity/Persona.cs
@@ -3,10 +3,11 @@ namespace GranjaAvicola.App.Domain
 {
     public class Persona
     {
-        public int id  {get;set;}
         public string Nombre {get;set;}
         public string Apellido {get;set;}
-        public string Telefono {get;set;}
+        public int Telefono {get;set;}
+        public string Correo {get;set;}
+        public genero Genero {get; set;}
     }
     
 }
