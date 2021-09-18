45baa0bdeb769c5e7475f351c6b5d963dab37526
Angel Gonzalez <angonzalezpa@unal.edu.co>
2021-09-17 20:11:37 -0500

    Primera integracion de las migraciones

---
+++ GranjaAvicola.App.Console/Console.cs
@@ -0,0 +1,11 @@
+﻿using GranjaAvicola.App.Domain;
+namespace GranjaAvicola.App.Console
+{
+    class Console
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("Hello World!");
+        }
+    }
+}
