a825b2227c4989a841a7a27c695de4dc0d9c23b1
Miguel Ángel Reyes Malvehy <reyesmalvehy@gmail.com>
2021-10-01 07:49:53 -0500

    Migrations

---
+++ GranjaAvicola.App.Persistent/Migrations/20210930152512_Inicial.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace GranjaAvicola.App.Persistence.Migrations
+{
+    public partial class Inicial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Diagnostico",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    ID_Sugerencia = table.Column<int>(type: "int", nullable: false),
+                    ID_VeterinarioCargo = table.Column<int>(type: "int", nullable: false),
+                    ID_Registro = table.Column<int>(type: "int", nullable: false),
+                    DiagnosticoVet = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Sugerencia = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Diagnostico", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Galpon",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Georeferencia = table.Column<int>(type: "int", nullable: false),
+                    ID_OperarioCargo = table.Column<int>(type: "int", nullable: false),
+                    ID_VeterinarioCargo = table.Column<int>(type: "int", nullable: false),
+                    Nombre = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    NumeroAnimales = table.Column<int>(type: "int", nullable: false),
+                    FechaIngreso = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    FechaSalida = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Galpon", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Georeferencias",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    latitud = table.Column<double>(type: "float", nullable: false),
+                    altitud = table.Column<double>(type: "float", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Georeferencias", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Persona",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nombre = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Apellido = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Telefono = table.Column<int>(type: "int", nullable: false),
+                    Correo = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Genero = table.Column<int>(type: "int", nullable: false),
+                    ID_Rol = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Persona", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Registro",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    ID_Galpon = table.Column<int>(type: "int", nullable: false),
+                    FechaRegistro = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Temperatura = table.Column<double>(type: "float", nullable: false),
+                    Agua = table.Column<double>(type: "float", nullable: false),
+                    Alimento = table.Column<double>(type: "float", nullable: false),
+                    PromedioHuevos = table.Column<int>(type: "int", nullable: false),
+                    GallinasEnfermas = table.Column<int>(type: "int", nullable: false),
+                    ID_Trabajador = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Registro", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Rol",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Descripcion = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Rol", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Diagnostico");
+
+            migrationBuilder.DropTable(
+                name: "Galpon");
+
+            migrationBuilder.DropTable(
+                name: "Georeferencias");
+
+            migrationBuilder.DropTable(
+                name: "Persona");
+
+            migrationBuilder.DropTable(
+                name: "Registro");
+
+            migrationBuilder.DropTable(
+                name: "Rol");
+        }
+    }
+}
